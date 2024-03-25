0443735c8320123348edf787907a1623a8686932
Marttins23 <mateus_martins18@yahoo.com.br>
2024-03-25 17:34:51 -0300

    Alterar layout dos cards de servico da tela inicial.

--- src/Components/CardServicos.js
+++ src/Components/CardServicos.js
@@ -12,46 +12,39 @@ class CardServicos extends HTMLElement {
     const componentRoot = document.createElement("div");
     componentRoot.setAttribute("class", "card border-0 rounded-4 shadow");
 
-    const cardHeader = document.createElement("div");
-    cardHeader.setAttribute("class", "card-header bg-verde-principal-2 text-white");
-
     const cardBody = document.createElement("div");
     cardBody.setAttribute("class", "card-body p-4");
 
-    const cardBodyTitle = document.createElement("div");
-    cardBodyTitle.setAttribute("class", "text-center");
+    const divIcon = document.createElement("div");
+    divIcon.setAttribute("class", "text-center bg-verde-principal-2 icon-container");
 
     const icon = document.createElement("i");
-    icon.setAttribute("class", `${this.getAttribute("icon-class")} fa-4x texto-verde-principal-1 p-3 bg-cinza-azul-50 rounded-4`);
+    icon.setAttribute("class", `${this.getAttribute("icon-class")} fa-4x text-white p-3 rounded-4`);
 
-    const title = document.createElement("h4");
-    title.setAttribute("class", `card-title fonte-primaria fw-bold mb-0`);
+    const title = document.createElement("h5");
+    title.setAttribute("class", `card-title fonte-primaria fw-bold pt-2 mb-0`);
     title.textContent = this.getAttribute("title");
 
     const description = document.createElement("p");
-    description.setAttribute("class", `card-text texto-justificado fonte-secundaria texto-cinza py-4 mb-0`);
+    description.setAttribute("class", `card-text texto-justificado fonte-secundaria texto-cinza pt-1 pb-4 mb-0`);
     description.textContent = this.getAttribute("description");
 
     const divButton = document.createElement("div");
-    divButton.setAttribute("class", "d-flex justify-content-center pt-2");
+    divButton.setAttribute("class", "d-flex justify-content-center");
 
     const button = document.createElement("button");
-    button.setAttribute("class", "btn btn-lg border-verde-principal-2 texto-verde-principal-2 fw-semibold rounded-4");
+    button.setAttribute("class", "btn border-verde-principal-2 texto-verde-principal-2 fw-semibold rounded-4");
     button.role = "button";
     button.href = "#!";
     button.textContent = "Ver Detalhes";
     
-    cardBodyTitle.appendChild(icon);
-    cardHeader.appendChild(title);
-    cardBody.appendChild(cardBodyTitle);
+    divIcon.appendChild(icon);
+    cardBody.appendChild(title);
     cardBody.appendChild(description);
     divButton.appendChild(button);
     cardBody.appendChild(divButton);
 
-    const cardFooter = document.createElement("div");
-    cardFooter.setAttribute("class", "card-footer bg-verde-principal-2");
-
-    componentRoot.appendChild(cardHeader);
+    componentRoot.appendChild(divIcon);
     componentRoot.appendChild(cardBody);
 
     return componentRoot;
@@ -68,8 +61,8 @@ class CardServicos extends HTMLElement {
       .card {
         max-width: 350px;
       }
-      
-      .card .card-header {
+
+      .card .icon-container {
         border-top-left-radius: 16px;
         border-top-right-radius: 16px;
       }
